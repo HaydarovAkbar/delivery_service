73262a89cafdf2523e9509a36f2c3757e6addd8c
HaydarovAkbar <khaydarov3255@gmail.com>
2023-11-23 16:25:58 +0500

    change db

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import TGUsers, Channel, Admin, Reklama, Stats, Products, Caption, ProductSize, Level
 
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'price')
+    list_per_page = 10
+
+
 admin.site.register(TGUsers)
 admin.site.register(Channel)
 admin.site.register(Admin)
---
+++ src/app/translation/translate.py
@@ -0,0 +1,14 @@
+from django.utils.translation import gettext_lazy as _
+from modeltranslation.translator import TranslationOptions, register
+
+from app.models import Products, ProductSize, TGUsers, Level
+
+
+@register(Products)
+class ProductsTranslationOptions(TranslationOptions):
+    fields = ('name', 'content')
+
+
+@register(ProductSize)
+class ProductSizeTranslationOptions(TranslationOptions):
+    fields = ('name',)
